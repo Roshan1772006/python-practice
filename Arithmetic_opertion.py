6c5d3b2c0d8f0e5efe689fce814d3952e091de35
Roshan1772006 <roshankaduskar6@gmail.com>
2026-07-01 17:56:08 +0530

    Add files via upload

---
+++ Arithmetic_opertion.py
@@ -0,0 +1,11 @@
+# arthimetic operation it use for integer
+# + ,- ,* ,/ , %
+
+a = 5
+b= 8
+print(a+b) # additon it return the sum
+print(a-b) # subtraction return the remaining value after minus from the value
+print(a*b) # Multiplication is return the product value
+print(a/b) # divsion is return the qoition
+print(a%b) # modules is return the remainder
+print(2**10) # Power it return the power of n^n , "**" is use to raise  or power of the number
